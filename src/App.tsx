a80102728b203141982aa3c9c1490a5aa1266a20
kachanovski03@gmail.com <13shurik03>
2021-03-23 17:57:57 +0300

    initial

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,48 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {ChangeEvent} from 'react';
 import './App.css';
 
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const [data, setData] = React.useState<Array<string>>([])
+    const [inputValue, setInputValue] = React.useState<string>('')
+    const [filter, setFilter] = React.useState<boolean>(false)
+
+
+    const onChangeInputValue = (e: ChangeEvent<HTMLInputElement>) => {
+        setInputValue(e.currentTarget.value)
+    }
+
+    const onChangeFilter = (e: ChangeEvent<HTMLInputElement>) => {
+        setFilter(e.currentTarget.checked)
+    }
+
+    const onClickNumber = () => {
+        if (typeof +inputValue === "number" && +inputValue > 0) {
+            setData(['dataArray'].filter(i => i.length > +inputValue))
+        }
+
+    }
+    const onClickString = () => {
+        if (inputValue !== '') {
+            setData(['dataArray'].filter(i => {
+                return filter ? i.toLocaleLowerCase().includes(inputValue.toLocaleLowerCase()) : i.includes(inputValue)
+            }))
+        }
+    }
+
+    return (
+        <div className="App">
+
+            <input value={inputValue} onChange={onChangeInputValue}/>
+            <input type={'checkbox'} checked={filter} onChange={onChangeFilter}/>
+            <button onClick={onClickNumber}>number</button>
+            <button onClick={onClickString}>string</button>
+            {data.map((i, id) => <div key={id}>{i}</div>)}
+        </div>
+    );
 }
 
 export default App;
+
+
